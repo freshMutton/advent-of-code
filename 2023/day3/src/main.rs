8dd2ff6beca0b4dd961ff7d81d1c4d7c4f14a440
Matt Sutton <matt@msutton.com.au>
2023-12-04 22:07:12 +1100

    day 3 part 2

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -12,11 +12,14 @@ fn main() {
     println!("second solution: {}", second_solution(&input));
 }
 
-fn first_solution(input: &Vec<String>) -> String {
-    let mut numbers = HashMap::new();
-    let mut symbols = HashSet::new();
+#[derive(Hash, Eq, PartialEq)]
+struct Position(isize, isize);
 
-    let mut search: Option<((isize, isize), (isize, isize), String)> = None;
+fn parse(input: &Vec<String>, is_symbol: fn(char) -> bool) -> (HashMap<(Position, Position), usize>, HashSet<Position>) {
+    let mut numbers : HashMap<(Position, Position), usize> = HashMap::new();
+    let mut symbols : HashSet<Position> = HashSet::new();
+
+    let mut search: Option<(Position, Position, String)> = None;
 
     for (y, line) in input.iter().enumerate() {
         for (x, c) in line.chars().enumerate() {
@@ -26,10 +29,10 @@ fn first_solution(input: &Vec<String>) -> String {
             match c {
                 _ if c.is_numeric() => {
                     if let Some(ref mut _search) = search {
-                        _search.1 = (x, y);
+                        _search.1 = Position(x, y);
                         _search.2.push(c);
                     } else {
-                        search = Some(((x, y), (x, y), c.to_string()));
+                        search = Some((Position(x, y), Position(x, y), c.to_string()));
                     }
                 },
                 _ => {
@@ -42,8 +45,8 @@ fn first_solution(input: &Vec<String>) -> String {
                         search = None;
                     }
 
-                    if c != '.' {
-                        symbols.insert((x, y));
+                    if is_symbol(c) {
+                        symbols.insert(Position(x, y));
                     }
                 },
             }
@@ -59,12 +62,18 @@ fn first_solution(input: &Vec<String>) -> String {
         }
     }
 
+    (numbers, symbols)
+}
+
+fn first_solution(input: &Vec<String>) -> String {
+    let (numbers, symbols) = parse(input, |c| c != '.');
+
     let sum = numbers.iter()
         .filter(|x| {
             for pos in x.0.0.0-1..=x.0.1.0+1 {
-                if  symbols.contains(&(pos, x.0.0.1-1)) || 
-                    symbols.contains(&(pos, x.0.0.1)) || 
-                    symbols.contains(&(pos, x.0.0.1+1)) 
+                if  symbols.contains(&Position(pos, x.0.0.1-1)) || 
+                    symbols.contains(&Position(pos, x.0.0.1)) || 
+                    symbols.contains(&Position(pos, x.0.0.1+1)) 
                 {
                     return true
                 }
@@ -79,5 +88,36 @@ fn first_solution(input: &Vec<String>) -> String {
 }
 
 fn second_solution(input: &Vec<String>) -> String {
-    format!("{}", "")
+    let (numbers, symbols) = parse(input, |c| c == '*');
+
+    let sum = numbers
+        .iter()
+        .fold(HashMap::new(), |mut acc, number| {
+            for x in number.0.0.0-1..=number.0.1.0+1 {
+                for y in number.0.0.1-1..=number.0.0.1+1 {
+                    let pos = Position(x, y);
+
+                    if symbols.contains(&pos) {
+                        acc.entry(pos)
+                            .and_modify(|x: &mut Vec<usize>| x.push(*number.1))
+                            .or_insert(vec!(*number.1));
+                    }
+                }
+            }
+
+            acc
+        })
+        .values()
+        .filter_map(|cogs| {
+            if cogs.len() == 2 {
+                return Some(cogs.iter().product::<usize>());
+            }
+
+            None
+        })
+        .sum::<usize>();
+
+
+
+    format!("{}", sum)
 }
